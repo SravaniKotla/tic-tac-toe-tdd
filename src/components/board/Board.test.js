ab640550fef7d2e4d164c2333766e765a5cda52e
SravaniKotla <skotla26@gmail.com>
2019-05-30 13:32:47 -0400

    test to check if board has rendered any square components

--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Board from './Board';
+import Square from '../square/Square';
 
 describe('Board Component', () => {
   let wrapper;
@@ -8,6 +9,10 @@ describe('Board Component', () => {
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render the Square Component', () => {
+    expect(wrapper.find('square').length).toEqual(9);
   });
 
 });
