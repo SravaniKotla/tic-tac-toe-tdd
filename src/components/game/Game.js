be0db33c73a80251d3ffe298f43ed7bcef1a484b
SravaniKotla <skotla26@gmail.com>
2019-05-29 21:31:03 -0400

    Game component shallow render passed

---
+++ src/components/game/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                
+            </div>
+        )
+    }
+}
+
+export default Game;
