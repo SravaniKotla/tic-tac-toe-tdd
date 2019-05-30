c6b3a2c478b8ba78af238951a293b9f6cb91a805
SravaniKotla <skotla26@gmail.com>
2019-05-30 14:00:29 -0400

    board component should render 9 square components test

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react'
 export default class Board extends Component {
     render() {
         return (
-            <div className="board">  
-            </div>
+            
+    <div className="board">
+      <h2>Tic-Tac-Toe</h2>
+      <div className="board-row">
+        
+      </div>
+      <div className="board-row">
+        
+      </div>
+      <div className="board-row">
+       
+      </div>
+    </div>
         )
     }
 }
--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -7,12 +7,16 @@ describe('Board Component', () => {
   let wrapper;
   beforeEach(() => wrapper = shallow(<Board />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render a <div className="board"/>', () => {
+    expect(wrapper.find('div.board').length).toEqual(1);
   });
 
-  it('should render the Square Component', () => {
-    expect(wrapper.find('square').length).toEqual(9);
+  it('should render 3 <div className="board-row"/>', () => {
+    expect(wrapper.find('div.board-row').length).toEqual(3);
+  });
+
+  it('should render 9 square components', () => {
+    expect(wrapper.find('Square').length).toEqual(9);
   });
 
 });
