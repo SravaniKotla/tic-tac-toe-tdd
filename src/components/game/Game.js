f1ae398d73a1b12053d7047ba4c4c1df08d6f274
SravaniKotla <skotla26@gmail.com>
2019-05-29 23:22:55 -0400

    Board component renders in the game component test passed

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Board from '../board/Board';
 
 class Game extends Component {
     render() {
         return (
             <div className="game">
-                
+                <Board />
             </div>
         )
     }
