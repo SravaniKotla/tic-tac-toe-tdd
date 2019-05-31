43024c6dfa7e279485169cfb79817f605919d2ae
SravaniKotla <skotla26@gmail.com>
2019-05-30 21:50:39 -0400

    board component couldnot render squares

--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -1,37 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Board from './Board';
-import Square from '../square/Square';
 
 describe('Board Component', () => {
   let wrapper;
-  beforeEach(() => wrapper = shallow(<Board />));
+  beforeEach(() => {
+    wrapper = shallow(
+      <Board
+        onClick={jest.fn()}
+        value={[]}
+      />
+    );
+  });
 
-  //snapshot testing
   it('should render correctly', () => expect(wrapper).toMatchSnapshot());
 
-  it('should render a <div className="board"/>', () => {
-    expect(wrapper.find('div.board').length).toEqual(1);
+  it('should render 4 div elements', () => {
+    expect(wrapper.find('div').length).toEqual(4);
   });
 
-  it('should render 3 <div className="board-row"/>', () => {
-    expect(wrapper.find('div.board-row').length).toEqual(3);
+  it('should render an instance of the Square component for each index of value', () => {
+    const value = Array(9).fill(null);
+    const totalSquares = value.length;
+    wrapper.setProps({ value });
+    expect(wrapper.find('Square').length).toEqual(totalSquares);
   });
-
-  it('should render 9 square components', () => {
-    expect(wrapper.find('Square').length).toEqual(9);
-  });
-
-});
-
-describe("Board Component props", () => {
-  
-  it('calls onClick event on click of a board sqaure', () => {
-    let squares = Array(9).fill(null);
-    const onClick = jest.fn();
-    let wrapper = mount(<Board squares={squares} onClick={onClick} />);
-    wrapper.find('button.square').first().simulate('click');
-    expect(onClick).toBeCalledWith(0);
-});
-
 });
