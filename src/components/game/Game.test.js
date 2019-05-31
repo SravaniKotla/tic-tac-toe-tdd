9bb0134babffa8f3cbdea6a4a08fa3ebdad67f58
SravaniKotla <skotla26@gmail.com>
2019-05-30 23:09:27 -0400

    game logic test passed

--- src/components/game/Game.test.js
+++ src/components/game/Game.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow , mount } from 'enzyme';
 import Game from './Game';
 import Board from '../board/Board';
 
@@ -28,5 +28,28 @@ describe('Game Component', () => {
     )).toEqual(true);
 
   });
+
+  it('renders game status correctly', () => {
+    wrapper = mount(<Game />);
+    const firstPlayer = wrapper.find('div.game-info').children().first().text();
+    expect(firstPlayer).toEqual('Player : X ');
+
+    const button = wrapper.find('button.square').first();
+    button.simulate('click');
+    const secondPlayer = wrapper.find('div.game-info').children().first().text();
+    expect(secondPlayer).toEqual('Player : O ');
+
+    const turn2 = wrapper.find('button.square').at(1);
+    turn2.simulate('click');
+    const turn3 = wrapper.find('button.square').at(4);
+    turn3.simulate('click');
+    const turn4 = wrapper.find('button.square').at(5);
+    turn4.simulate('click');
+    const turn5 = wrapper.find('button.square').at(8);
+    turn5.simulate('click');
+
+    const winner = wrapper.find('div.game-info').children().first().text();
+    expect(winner).toEqual('Winner : X');
+});
 
 });
